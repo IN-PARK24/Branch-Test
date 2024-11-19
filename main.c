ffd7942b4243a96237f7accf756ec35b23664a45
IN-PARK24 <inpark24@artware.co.kr>
2024-11-19 13:59:24 +0900

    1-2

--- main.c
+++ main.c
@@ -4,9 +4,9 @@ int main(void)
 {
 	int Test = 0;
 
-	Test = 123;
+	Test = 124;
 	printf("First Branch!!!!\n\r");
-	printf("First Branch 11111!!!!\n\r");
+	printf("First Branch 1 - 2!!!!\n\r");
 	printf("Test = %02d\n\r", Test);
 
 
